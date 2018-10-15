3395b7d28b9b163ab9e9372c7fed5d0971a78c57
Dimsol <claudiosakaeshigemi@gmail.com>
2018-10-15 20:47:55 -0300

    Aulas 8 completa
    Aulas 8 completa

--- meuPrimeiroProjeto/meuPrimeiroProjeto/urls.py
+++ meuPrimeiroProjeto/meuPrimeiroProjeto/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import hello, articles
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello),
+    path('articles/<int:year>/', articles),
 ]
---
+++ meuPrimeiroProjeto/meuPrimeiroProjeto/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse(' Olá mundo!')
+
+
+def articles(request, year):
+    return HttpResponse(' O ano enviado foi: '+ str(year))
